de8a65c56b043b20c0575f5965bf6a988fa0df07
Maurizio Carlotta <mauricarlottat@gmail.com>
2024-05-08 11:01:17 +0200

    Added editor directives

--- Assets/Scripts/Framework/ArtifaxScriptablePaths.cs
+++ Assets/Scripts/Framework/ArtifaxScriptablePaths.cs
@@ -1,8 +1,10 @@
 namespace Artifax.Framework
 {
+#if UNITY_EDITOR
     public class ArtifaxScriptablePaths
     {
         public const string GAME_EVENT_SCRIPTABLE_PATH = "Artifax/Events/";
         public const string VARIABLES_SCRIPTABLE_PATH = "Artifax/Variables/";
     }
+#endif
 }
