ad7ada92a4d5c99cb1751f4f4a69a97400734a13
Maurizio Carlotta <mauricarlottat@gmail.com>
2024-05-29 15:03:44 +0200

    Improved movement with invisible joystick

--- Assets/Scripts/Gameplay/Character/CharacterController2D.cs
+++ Assets/Scripts/Gameplay/Character/CharacterController2D.cs
@@ -8,15 +8,16 @@ namespace Artifax.ProjectBlock.Gameplay
     [RequireComponent(typeof(Rigidbody2D))]
     public class CharacterController2D : MonoBehaviour
     {
+        [SerializeField] private float m_MaxVelocity = 2f;
         [SerializeField] private Rigidbody2D m_Rigidbody2D;
-        [SerializeField] private float m_JumpForce = 400f;                          // Amount of force added when the player jumps.
-        [Range(0, .3f)][SerializeField] private float m_MovementSmoothing = .05f;   // How much to smooth out the movement
-        [SerializeField] private LayerMask m_WhatIsGround;                          // A mask determining what is ground to the character
-        [SerializeField] private Transform m_GroundCheck;                           // A position marking where to check if the player is grounded.
+        [SerializeField] private float m_JumpForce = 400f;
+        [Range(0, .3f)][SerializeField] private float m_MovementSmoothing = .05f;
+        [SerializeField] private LayerMask m_WhatIsGround;
+        [SerializeField] private Transform m_GroundCheck;
 
-        const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
-        private bool m_Grounded;            // Whether or not the player is grounded.
-        private bool m_FacingRight = true;  // For determining which way the player is currently facing.
+        const float k_GroundedRadius = .2f;
+        private bool m_Grounded;
+        private bool m_FacingRight = true;
         private Vector3 m_Velocity = Vector3.zero;
 
         [Header("Events")]
@@ -47,24 +48,24 @@ namespace Artifax.ProjectBlock.Gameplay
         }
 
 
-        public void Move(float move)
+        public void Move(float direction)
         {
             //only control the player if grounded or airControl is turned on
             if (m_Grounded)
             {
                 // Move the character by finding the target velocity
-                Vector3 targetVelocity = new Vector2(move * 10f, m_Rigidbody2D.velocity.y);
+                Vector3 targetVelocity = new Vector2(direction * m_MaxVelocity, m_Rigidbody2D.velocity.y);
                 // And then smoothing it out and applying it to the character
                 m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
 
                 // If the input is moving the player right and the player is facing left...
-                if (move > 0 && !m_FacingRight)
+                if (direction > 0 && !m_FacingRight)
                 {
                     // ... flip the player.
                     Flip();
                 }
                 // Otherwise if the input is moving the player left and the player is facing right...
-                else if (move < 0 && m_FacingRight)
+                else if (direction < 0 && m_FacingRight)
                 {
                     // ... flip the player.
                     Flip();
